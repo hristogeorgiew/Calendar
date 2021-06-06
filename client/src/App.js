4f9b344d80e1848844a527cf4749d1d89f2498ce
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-06-06 10:46:59 +0300

    Stored in locale storage and save state of Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import Day from './components/Day/Day';
 
 function App() {
+
+  const [nav, setNav] = useState(0);
+  const [days, setDays] = useState([]);
+  const [dateDisplay, setDateDisplay] = useState('');
+  const [clicked, setClicked] = useState();
+  const [events, setEvents] = useState(
+    localStorage.getItem('events') ? 
+      JSON.parse(localStorage.getItem('events')) : 
+        []
+  );
+
+  const eventForDate = date => events.find(e => e.date === date);
+
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(events));
+  }, [events]);
+
   return (
     <div className="App">
-      
+      <Day />
     </div>
   );
 }
---
+++ client/src/components/Day/Day.js
@@ -0,0 +1,8 @@
+
+const Day = () => {
+    return(
+        <h1>Hello from The Day Component</h1>
+    )
+}
+
+export default Day;
